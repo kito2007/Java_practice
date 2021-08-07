941418c8b36e90cbb56b9a653a0a4550d61f2344
kito2007 <60645183+kito2007@users.noreply.github.com>
2021-08-07 12:21:02 +0900

    메서드 연습

---
+++ Grammar_practice/src/pr01/Info.java
@@ -0,0 +1,9 @@
+package pr01;
+
+public class Info {
+	String t;
+	public int set(int t) {
+		System.out.print("당신이 입력한 숫자는 ");
+		return t;
+	}
+}
---
+++ Grammar_practice/src/pr01/Start.java
@@ -0,0 +1,18 @@
+package pr01;
+
+import java.util.Scanner;
+import java.io.IOException;
+public class Start {
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		int i;
+		System.out.print("숫자를 입력하세요: ");
+		i = sc.nextInt();
+		
+		Info t = new Info();
+		t.t = "아마도 ";
+		System.out.println(t.t + t.set(i)+"일 것이다.");
+		
+		
+	}
+}
